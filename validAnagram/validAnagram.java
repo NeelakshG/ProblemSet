9a3ab5b6e49607f8e1e5a6da01dca0a79913d831
neelaksh <Neelaksh.gupta05@gmail.com>
2025-10-20 18:30:38 -0400

    (2/150) valid anagram

--- validAnagram/validAnagram.java
+++ validAnagram/validAnagram.java
@@ -1,5 +1,29 @@
 package ProblemSet.validAnagram;
 
 public class validAnagram {
+        public boolean anagram(String word1, String word2) {
+
+            if (word1.length() != word2.length()) {
+                return false;
+            }
+
+            int[] charCounts = new int[26];
+            
+            for (int i = 0; i < word1.length(); i++) { //loop through each letter
+                //map each character into the array by subtracting it be the biggest value for lower case ascii
+                charCounts[word1.charAt(i) - 'a']++;    
+                charCounts[word2.charAt(i) - 'a']--;
+                }
+            
+            //check to see if any letter isn't accounted for
+            for (int count : charCounts){
+                if (count != 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
     
 }
